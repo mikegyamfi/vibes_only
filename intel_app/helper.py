a962fa596409e6928c37370cc9742ff7b9a20224
mikegyamfi <codeage20@gmail.com>
2024-11-04 01:24:10 +0000

    connect database

--- intel_app/helper.py
+++ intel_app/helper.py
@@ -36,7 +36,7 @@ import secrets
 
 def ref_generator(length=30):
     # Generate a random hexadecimal string with the specified length
-    return secrets.token_hex(length // 2).upper()  # Divide by 2 because each hex char represents 4 bits
+    return f"DT-{secrets.token_hex(length // 2).upper()}-VBZ"  # Divide by 2 because each hex char represents 4 bits
 
 def top_up_ref_generator(length=25):
     return f"TOPUP-{secrets.token_hex((length - 6) // 2).upper()}"
